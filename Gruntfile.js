94395cc3f90cb2a8c06a42cd87cd1fb9430cffd2
brenden <rstone770@gmail.com>
2013-10-24 14:48:49 -0700

    removed grunt-mocha

--- Gruntfile.js
+++ Gruntfile.js
@@ -92,7 +92,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-closure-compiler');
   grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-mocha');
   grunt.loadNpmTasks('grunt-nodestatic');
 
 
