cc3b33f56d9e79d24cf260d95d867b793846f771
brenden <rstone770@gmail.com>
2013-11-25 15:17:41 -0700

    fixed index.html

--- app/auto-man/auto-man.js
+++ app/auto-man/auto-man.js
@@ -4,8 +4,4 @@ goog.provide('AutoMan.start');
 goog.require('AutoMan.boot');
 goog.require('AutoMan.ui');
 
-var module = module || {};
-
 AutoMan.start = function(options) {};
-
-module.exports = AutoMan;
