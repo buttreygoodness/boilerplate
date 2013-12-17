a58949654e5235e9e4f788186be994e9a34d389d
mittens <rstone770@gmail.com>
2013-12-16 22:29:33 -0700

    lint

--- tasks/config/test/coverage.js
+++ tasks/config/test/coverage.js
@@ -3,10 +3,9 @@ var config = {
   src: ['app/auto-man/**/*.js'],
   options: {
     specs: ['test/spec/**-test.js'],
-    template: require('grunt-template-jasmine-istanbul'),
-    specs: ['test/spec/**/*-test.js'],
     vendor: ['app/lib/closure-library/closure/goog/base.js', 'test/helpers/closure.js'],
     helpers: ['bin/fixture-deps.js', 'bin/automan-deps.js', 'app/lib/chai/chai.js', 'test/helpers/chai.js'],
+    template: require('grunt-template-jasmine-istanbul'),
     keepFile: true,
     templateOptions: {
       coverage: 'test/coverage/coverage.json',
