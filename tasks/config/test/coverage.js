ca398f0e63fc0da522618dc5ce19439a685a2850
mittens <rstone770@gmail.com>
2013-12-17 00:14:04 -0700

    made coverage a bitch.

--- tasks/config/test/coverage.js
+++ tasks/config/test/coverage.js
@@ -2,14 +2,19 @@ var config = {
   taskName: 'jasmine.coverage',
   src: ['app/auto-man/**/*.js'],
   options: {
-    specs: ['test/spec/**-test.js'],
+    specs: ['test/spec/**/*-test.js'],
     vendor: ['app/lib/closure-library/closure/goog/base.js', 'test/helpers/closure.js'],
     helpers: ['bin/fixture-deps.js', 'bin/automan-deps.js', 'app/lib/chai/chai.js', 'test/helpers/chai.js'],
     template: require('grunt-template-jasmine-istanbul'),
-    keepFile: true,
     templateOptions: {
       coverage: 'test/coverage/coverage.json',
-      report: 'test/coverage'
+      report: 'test/coverage',
+      thresholds: {
+        lines: 90,
+        statements: 90,
+        branches: 90,
+        functions: 90
+      }
     }
   }
 };
